31b5b1bd525247f118ec331a581f489f56f94c40
crisrud <cristiano.scarlat@hotmail.com>
2025-06-11 21:33:33 -0300

    Criacao dos metodos SignUp e SignIn

--- lib/plan_market_auth.dart
+++ lib/plan_market_auth.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 /// A Calculator.
 class PlanMarketAuth with ChangeNotifier {
@@ -12,19 +15,55 @@ class PlanMarketAuth with ChangeNotifier {
   final _resource = "/v1/accounts";
   final apikey = 'AIzaSyDek1Qb1p8EPhZZZ8GtbGWjic62awCfUNc';
 
-  get mensagemErro {
-    return _mensagem;
+
+  Future<bool> signUp(String email, String pass) async {
+    String sUri = '$_url$_resource:signUp?key=$apikey';
+    Uri uri = Uri.parse(sUri);
+    print(uri);
+    print(email);
+    print(pass);
+    var response = await http.post(uri, body: {
+      'email': email,
+      'password': pass,
+      'returnSecureToken': 'true'
+    });
+
+    if (response.statusCode == 200) {
+      _mensagem = 'Usuário criado com sucesso';
+
+      var responseData = jsonDecode(response.body);
+      token = responseData['idToken'];
+      return true;
+    }else {
+      final responseData = jsonDecode(response.body);
+      _mensagem = responseData['error']['message'];
+      return false;
+    }
+
   }
 
-  void signOut() {
-    token = null;
-    _mensagem = null;
-    notifyListeners();
-  }
+  Future<bool> signIn(String email, String senha) async {
+    String sUri = '$_url$_resource:signInWithPassword?key=$apikey';
+    Uri uri = Uri.parse(sUri);
+    var response = await http.post(
+        uri,
+        body: {
+          'email': email,
+          'password': senha,
+          'returnSecureToken': 'true',
+        });
+    if (response.statusCode == 200) {
+      _mensagem = 'Usuário criado com sucesso';
 
-  void signIn(String email, String senha) {
-    // Implement signIn logic here
-  }
+      final responseData = json.decode(response.body);
+      token = responseData['idToken'];
+      notifyListeners();
+      return true;
+    } else {
+      final responseData = json.decode(response.body);
+      _mensagem = responseData['error']['message'];
+      return false;
+    }
 
-  // Implement signUp and signIn methods here
+  }
 }
